9cd8f17bd323149ae39fecfec6711eefaa3d42e6
itagaki <itagaki.shintaro@gmail.com>
2018-08-17 10:58:05 +0900

    add extract skills

--- src/skill-view.js
+++ src/skill-view.js
@@ -1,6 +1,8 @@
 import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
 import handleImage from './util/ImageHandler.js';
 import './shared-styles.js';
+import '@polymer/paper-button/paper-button.js';
+import skills from './util/Skills.js';
 
 class SkillView extends PolymerElement {
     static get template() {
@@ -9,7 +11,13 @@ class SkillView extends PolymerElement {
         </style>
 
         <div class="container">
+            <paper-button raised class="on" on-click="getWords">スキル抽出</paper-button>
 
+            <template is="dom-repeat" items="{{mySkills}}">
+                <ul>
+                    <li>{{item}}</li>
+                </ul>
+            </template>
         </div>
         `;
     }
@@ -17,7 +25,7 @@ class SkillView extends PolymerElement {
     constructor() {
         console.log( 'constructor()' );
         super();
-        this.callNL();
+        this.mySkills = [];
     }
 
     static get properties() {
@@ -26,21 +34,37 @@ class SkillView extends PolymerElement {
         }
     }
 
-    callNL() {
+    getWords() {
+        firebase.database().ref( 'comments/user:' + this.user.uid ).once( 'value' ).then( snapshot => {
+            let comments = snapshot.val();
+            let text = '';
+            Object.keys( comments ).forEach( key => {
+                text = text + '\n' + comments[key].text;
+            });
+            this.callNLPSyntax( text );
+        } );
+    }
+
+    callNLPSyntax( text ) {
         const url = 'https://us-central1-buddyup-204005.cloudfunctions.net/NLP-syntax';
-        let data = {
-            message: '私はもうJavaなんか嫌い。JavaScriptが好きになったの。'
-        }
+        let data = { message: text };
         fetch(url, {
             body: JSON.stringify(data),
             method: 'POST'
         }).then( response => {
             return response.json();
         }).then( json => {
-            console.log(json);
+            this.getSkills(json[0].tokens);
         });
     }
 
+    getSkills( tokens ) {
+        let nouns = tokens
+            .filter( v => v.partOfSpeech.tag === 'NOUN' ) // 名詞のみにフィルター
+            .map( v => v.lemma.toLowerCase() ) // 語幹を抽出（英語のときの活用などが原型になる）
+            .filter( (x, i, self) => self.indexOf(x) === i ); // 重複排除
+        this.mySkills = nouns.filter( v => skills.includes( v ) );
+    }
 
 }
 
---
+++ src/util/Skills.js
@@ -0,0 +1,6 @@
+const skills = [
+    'java',
+    'javascript',
+    'ruby'
+];
+export default skills
