151187b2bc0c1e9fd3f4a248586d8f96b93e7797
itagakishintaro <itagaki.shintaro@gmail.com>
2018-11-19 21:07:05 +0900

    add buddyup candidates

--- src/users-view.js
+++ src/users-view.js
@@ -1,4 +1,4 @@
-import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
+import {PolymerElement, html} from '@polymer/polymer/polymer-element.js';
 import SKILLS from './util/Skills.js';
 import './shared-styles.js';
 import './loading-view.js';
@@ -10,8 +10,8 @@ import '@polymer/iron-icon/iron-icon.js';
 import '@polymer/iron-icons/iron-icons.js';
 
 class UsersView extends PolymerElement {
-    static get template() {
-        return html `
+  static get template() {
+    return html `
         <style include="shared-styles">
           .on {
             margin-bottom: 1em;
@@ -42,25 +42,9 @@ class UsersView extends PolymerElement {
           <paper-button raised class="on" on-click="getUsers">ユーザー一覧を表示</paper-button>
 
           <paper-dialog id="dialog" class="dialog">
-              <template is="dom-repeat" items="{{relatedComments}}">
-                <div>{{item}}</div>
-              </template>
-
-              <template is="dom-if" if="{{sameSkillHolders.length}}">
-                <hr>
-                <p>同じワードを持つ人</p>
-              </template>
-              <template is="dom-repeat" items="{{sameSkillHolders}}">
-                <div>
-                    <template is="dom-if" if="{{item.photoURL}}">
-                        <img src="{{item.photoURL}}" class="icon">
-                     </template>
-                     <template is="dom-if" if="{{!item.photoURL}}">
-                        <img src="images/manifest/icon-48x48.png" class="icon">
-                     </template>
-                     <span>{{item.displayName}}</span>
-                </div>
-              </template>
+            <template is="dom-repeat" items="{{relatedComments}}">
+              <div>{{item}}</div>
+            </template>
           </paper-dialog>
 
           <!-- 自分 -->
@@ -81,6 +65,27 @@ class UsersView extends PolymerElement {
                 </div>
             </template>
           </div>
+
+          <!-- Buddyup候補 -->
+          <template is="dom-if" if="{{ candidates.length }}">
+            <hr>
+            <div>
+              <template is="dom-repeat" items="{{candidates}}" as="candidate">
+                <div>「{{ candidate.skill }}」でBuddyup!</div>
+                <template is="dom-repeat" items="{{candidate.buddies}}">
+                  <div>
+                    <template is="dom-if" if="{{item.photoURL}}">
+                        <img src="{{item.photoURL}}" class="icon">
+                     </template>
+                     <template is="dom-if" if="{{!item.photoURL}}">
+                        <img src="images/manifest/icon-48x48.png" class="icon">
+                     </template>
+                     <span>{{item.displayName}}</span>
+                  </div>
+                </template>
+              </template>
+            </div>
+          </template>
 
           <hr>
           <div on-click="toggleFriends">
@@ -151,213 +156,211 @@ class UsersView extends PolymerElement {
 
         <loading-view display="{{loadingDisplay}}"></loading-view>
         `;
-    }
+  }
 
-    constructor() {
-        console.log( 'constructor()' );
-        super();
-        this.user = {};
-        this.friendUIDs = [];
-        this.friends = [];
-        this.others = [];
-        this.loadingDisplay = 'none';
-        this.target = {};
-        this.toastText = "更新しました!";
-    }
+  constructor() {
+    console.log('constructor()');
+    super();
+    this.user = {};
+    this.friendUIDs = [];
+    this.friends = [];
+    this.others = [];
+    this.loadingDisplay = 'none';
+    this.target = {};
+    this.toastText = '更新しました!';
+    this.candidates = [];
+  }
 
-    ready() {
-      super.ready();
-      this.$.friends.opened = true;
-      this.friendsOpened = this.$.friends.opened;
-      this.othersOpened = this.$.others.opened;
-    }
+  ready() {
+    super.ready();
+    this.$.friends.opened = true;
+    this.friendsOpened = this.$.friends.opened;
+    this.othersOpened = this.$.others.opened;
+  }
 
-    static get properties() {
-        return {
-            user: Object
+  static get properties() {
+    return {user: Object}
+  }
+
+  getUsers() {
+    let getProfilesPromise = new Promise((resolve, reject) => {
+      this._getProfiles(resolve, reject);
+    });
+
+    let getFriendsPromise = new Promise((resolve, reject) => {
+      this._getFriends(resolve, reject);
+    });
+
+    getProfilesPromise.then(() => {
+      getFriendsPromise.then(() => {
+        this._getOthers();
+        this.setBuddyupCandidates(this.user.skills);
+      });
+    });
+  }
+
+  // Profilesを取得する
+  _getProfiles(resolve, reject) {
+    firebase.database().ref('profiles/').once('value').then(snapshot => {
+      this.profiles = snapshot.val();
+      resolve();
+    });
+  }
+
+  // 知り合い(friend)の定義はpartyに一緒に参加した人
+  _getFriends(resolve, reject) {
+    this.loadingDisplay = 'block';
+    firebase.database().ref('parties/').once('value').then(snapshot => {
+      let parties = snapshot.val();
+      let friendUIDs = [];
+      Object.keys(parties).forEach(key => {
+        if (parties[key].members && Object.keys(parties[key].members).includes(this.user.uid)) { // 自分が含まれているなら
+          friendUIDs = friendUIDs.concat(Object.keys(parties[key].members));
         }
+      });
+      friendUIDs = friendUIDs.filter((x, i, self) => self.indexOf(x) === i); // 重複排除
+      friendUIDs = friendUIDs.filter(x => x !== this.user.uid); // 自分を削除
+
+      this.friendUIDs = friendUIDs;
+      this.friends = this.friendUIDs.map(key => {
+        let v = this.profiles[key];
+        v.uid = key;
+        return v;
+      });
+    }). finally(() => {
+      resolve();
+      this.loadingDisplay = 'none';
+    });
+  }
+
+  _getOthers() {
+    this.loadingDisplay = 'block';
+    this.others = Object.keys(this.profiles).filter(key => key !== this.user.uid && !this.friendUIDs.includes(key)). // 自分でも知り合いでもない
+    map(key => {
+      let v = this.profiles[key];
+      v.uid = key;
+      return v;
+    });
+
+    this.loadingDisplay = 'none';
+  }
+
+  showSkills(e) {
+    this.skills = [];
+    this.target.uid = e.target.dataset.uid;
+    this.getSkills();
+  }
+
+  showComments(e) {
+    this.target.skill = e.target.innerText;
+
+    if (this.target.uid !== e.target.parentNode.dataset.uid) {
+      this.target.uid = e.target.parentNode.dataset.uid;
+
+      this._getCommentsText().then(() => {
+        this.relatedComments = this.comments.map(c => c.text.toLowerCase()).filter(text => text.indexOf(this.target.skill) >= 0);
+        this.$.dialog.open();
+      });
+    } else {
+      this.relatedComments = this.comments.map(c => c.text.toLowerCase()).filter(text => text.indexOf(this.target.skill) >= 0);
+      this.$.dialog.open();
     }
+  }
 
-    getUsers() {
-        let getFriendsPromise = new Promise( ( resolve, reject ) => {
-            this._getFriends( resolve, reject );
-        } );
-        getFriendsPromise.then( () => {
-            this._getOthers();
-        } );
+  getSkills() {
+    this.loadingDisplay = 'block';
+    this._getCommentsText().then(text => this._callNLPSyntax(text)).then(tokens => this._extractSkills(tokens)).then(skills => {
+      if (this.target.uid === this.user.uid) {
+        this.user.skills = skills;
+        this.notifyPath('user.skills');
+      } else if (this.friendUIDs.includes(this.target.uid)) {
+        this.friends.forEach((o, i) => {
+          if (o.uid === this.target.uid) {
+            this.friends[i].skills = skills;
+            this.notifyPath('friends.' + i + '.skills');
+          }
+        });
+      } else {
+        this.others.forEach((o, i) => {
+          if (o.uid === this.target.uid) {
+            this.others[i].skills = skills;
+            this.notifyPath('others.' + i + '.skills');
+          }
+        });
+      }
+      if (skills) {
+        this.toastText = "更新しました！"
+        firebase.database().ref('profiles/' + this.target.uid + '/skills').set(skills);
+        this.setBuddyupCandidates(skills);
+      } else {
+        this.toastText = "現在のワードリストに一致するコメントがみつかりませんでした。"
+      }
+    }). finally(() => {
+      this.$.toast.open();
+      this.loadingDisplay = 'none';
+    });
+  }
+
+  _getCommentsText() {
+    return firebase.database().ref('comments/' + this.target.uid).once('value').then(snapshot => {
+      if (!snapshot.val()) {
+        return;
+      }
+      this.comments = Object.keys(snapshot.val()).map(key => snapshot.val()[key]). // Objectから配列に
+      filter(comment => comment.text); // 空のコメントを削除
+      let text = '';
+      this.comments.forEach(c => {
+        text = text + '\n' + c.text.toLowerCase();
+      });
+      return text;
+    });
+  }
+
+  _callNLPSyntax(text) {
+    if (!text) {
+      return;
     }
+    const url = 'https://us-central1-buddyup-204005.cloudfunctions.net/NLP-syntax';
+    let data = {
+      message: text
+    };
+    return fetch(url, {
+      body: JSON.stringify(data),
+      method: 'POST'
+    }).then(response => response.json()).then(json => json[0].tokens);
+  }
 
-    // 知り合い(friend)の定義はpartyに一緒に参加した人
-    _getFriends( resolve, reject ) {
-        this.loadingDisplay = 'block';
-        firebase.database().ref( 'profiles/' ).once( 'value' ).then( snapshot => {
-            this.profiles = snapshot.val();
-
-            firebase.database().ref( 'parties/' ).once( 'value' )
-                .then( snapshot => {
-                    let parties = snapshot.val();
-                    let friendUIDs = [];
-                    Object.keys( parties ).forEach( key => {
-                        if ( parties[ key ].members && Object.keys( parties[ key ].members ).includes( this.user.uid ) ) { // 自分が含まれているなら
-                            friendUIDs = friendUIDs.concat( Object.keys( parties[ key ].members ) );
-                        }
-                    } );
-                    friendUIDs = friendUIDs.filter( ( x, i, self ) => self.indexOf( x ) === i ); // 重複排除
-                    friendUIDs = friendUIDs.filter( x => x !== this.user.uid ); // 自分を削除
-
-                    this.friendUIDs = friendUIDs;
-                    this.friends = this.friendUIDs.map( key => {
-                        let v = this.profiles[ key ];
-                        v.uid = key;
-                        return v;
-                    } );
-                } ).finally( () => {
-                    resolve();
-                    this.loadingDisplay = 'none';
-                } );
-        } );
+  _extractSkills(tokens) {
+    if (!tokens) {
+      return;
     }
+    let nouns = tokens.filter(v => ['NOUN', 'X'].includes(v.partOfSpeech.tag)). // 名詞とその他のみにフィルター
+    map(v => v.lemma). // 語幹を抽出（英語のときの活用などが原型になる）
+    filter(x => x.length > 1). // 1文字を排除
+    filter((x, i, self) => self.indexOf(x) === i); // 重複排除
+    return nouns.filter(v => SKILLS.includes(v));
+  }
 
-    _getOthers() {
-        this.loadingDisplay = 'block';
-        // firebase.database().ref( 'profiles/' ).once('value').then( snapshot => {
-        this.others = Object.keys( this.profiles )
-            .filter( key => key !== this.user.uid && !this.friendUIDs.includes( key ) ) // 自分でも知り合いでもない
-            .map( key => {
-                let v = this.profiles[ key ];
-                v.uid = key;
-                return v;
-            } );
+  setBuddyupCandidates(skills) {
+    let users = [].concat(this.friends).concat(this.others);
+    skills.forEach( skill => {
+      let buddies = users.filter(u => (u.skills && u.skills.includes(skill)));
+      if ( buddies.length >= 2 ){ // 自分を含めて3人になれば候補
+        this.push('candidates', { skill, buddies });
+      }
+    });
+  }
 
-        this.loadingDisplay = 'none';
-        // });
-    }
+  toggleFriends() {
+    this.$.friends.toggle();
+    this.friendsOpened = this.$.friends.opened;
+  }
 
-    showSkills( e ) {
-        this.skills = [];
-        this.target.uid = e.target.dataset.uid;
-        this.getSkills();
-    }
-
-    showComments( e ) {
-        this.target.skill = e.target.innerText;
-
-        if ( this.target.uid !== e.target.parentNode.dataset.uid ) {
-            this.target.uid = e.target.parentNode.dataset.uid;
-
-            this._getCommentsText().then( () => {
-                this.relatedComments = this.comments
-                    .map( c => c.text.toLowerCase() )
-                    .filter( text => text.indexOf( this.target.skill ) >= 0 );
-                this.$.dialog.open();
-            } );
-        } else {
-            this.relatedComments = this.comments
-                .map( c => c.text.toLowerCase() )
-                .filter( text => text.indexOf( this.target.skill ) >= 0 );
-            this.$.dialog.open();
-        }
-
-        this.showSameSkillHolders( e.target.innerText );
-    }
-
-    getSkills() {
-        this.loadingDisplay = 'block';
-        this._getCommentsText()
-            .then( text => this._callNLPSyntax( text ) )
-            .then( tokens => this._extractSkills( tokens ) )
-            .then( skills => {
-                if ( this.target.uid === this.user.uid ) {
-                    this.user.skills = skills;
-                    this.notifyPath( 'user.skills' );
-                } else if ( this.friendUIDs.includes( this.target.uid ) ) {
-                    this.friends.forEach( ( o, i ) => {
-                        if ( o.uid === this.target.uid ) {
-                            this.friends[ i ].skills = skills;
-                            this.notifyPath( 'friends.' + i + '.skills' );
-                        }
-                    } );
-                } else {
-                    this.others.forEach( ( o, i ) => {
-                        if ( o.uid === this.target.uid ) {
-                            this.others[ i ].skills = skills;
-                            this.notifyPath( 'others.' + i + '.skills' );
-                        }
-                    } );
-                }
-                if ( skills ) {
-                    this.toastText = "更新しました！"
-                    firebase.database().ref( 'profiles/' + this.target.uid + '/skills' ).set( skills );
-                } else {
-                    this.toastText = "現在のワードリストに一致するコメントがみつかりませんでした。"
-                }
-            } )
-            .finally( () => {
-                this.$.toast.open();
-                this.loadingDisplay = 'none';
-            } );
-    }
-
-    _getCommentsText() {
-        return firebase.database().ref( 'comments/' + this.target.uid ).once( 'value' ).then( snapshot => {
-            if ( !snapshot.val() ) {
-                return;
-            }
-            this.comments = Object.keys( snapshot.val() )
-                .map( key => snapshot.val()[ key ] ) // Objectから配列に
-                .filter( comment => comment.text ); // 空のコメントを削除
-            let text = '';
-            this.comments.forEach( c => {
-                text = text + '\n' + c.text.toLowerCase();
-            } );
-            return text;
-        } );
-    }
-
-    _callNLPSyntax( text ) {
-        if ( !text ) {
-            return;
-        }
-        const url = 'https://us-central1-buddyup-204005.cloudfunctions.net/NLP-syntax';
-        let data = { message: text };
-        return fetch( url, {
-                body: JSON.stringify( data ),
-                method: 'POST'
-            } )
-            .then( response => response.json() )
-            .then( json => json[ 0 ].tokens );
-    }
-
-    _extractSkills( tokens ) {
-        if ( !tokens ) {
-            return;
-        }
-        let nouns = tokens
-            .filter( v => [ 'NOUN', 'X' ].includes( v.partOfSpeech.tag ) ) // 名詞とその他のみにフィルター
-            .map( v => v.lemma ) // 語幹を抽出（英語のときの活用などが原型になる）
-            .filter( x => x.length > 1 ) // 1文字を排除
-            .filter( ( x, i, self ) => self.indexOf( x ) === i ); // 重複排除
-        return nouns.filter( v => SKILLS.includes( v ) );
-    }
-
-    showSameSkillHolders( skill ) {
-        let users = [].concat( this.friends ).concat( this.others ).concat( [ this.user ] );
-        users = users.filter( u => u.uid !== this.target.uid );
-        this.sameSkillHolders = users.filter( u => ( u.skills && u.skills.includes( skill ) ) );
-    }
-
-    toggleFriends() {
-      this.$.friends.toggle();
-      this.friendsOpened = this.$.friends.opened;
-    }
-
-    toggleOthers() {
-      this.$.others.toggle();
-      this.othersOpened = this.$.others.opened;
-    }
-
-
+  toggleOthers() {
+    this.$.others.toggle();
+    this.othersOpened = this.$.others.opened;
+  }
 
 }
 
-window.customElements.define( 'users-view', UsersView );
+window.customElements.define('users-view', UsersView);
