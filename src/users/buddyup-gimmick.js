6b646d835e3841d161401284eb6892b01574bc3e
itagakishintaro <itagaki.shintaro@gmail.com>
2018-12-11 15:28:10 +0900

    close #5

--- src/users/buddyup-gimmick.js
+++ src/users/buddyup-gimmick.js
@@ -1,14 +1,22 @@
 import { PolymerElement, html } from "@polymer/polymer/polymer-element.js";
 import "../shared-styles.js";
 import "@polymer/paper-dialog/paper-dialog.js";
+import "@polymer/paper-button/paper-button.js";
 
 class BuddyupGimmick extends PolymerElement {
   static get template() {
     return html`
-      <style include="shared-styles"></style>
+      <style include="shared-styles">
+        .action {
+          text-align: center;
+        }
+      </style>
 
       <paper-dialog id="dialog">
         <div>{{skill}}でBuddyup!しました</div>
+        <div class="action">
+          <paper-button raised on-click="ok">OK</paper-button>
+        </div>
       </paper-dialog>
     `;
   }
@@ -30,6 +38,11 @@ class BuddyupGimmick extends PolymerElement {
     if (newValue && this.skill) {
       this.$.dialog.open();
     }
+  }
+
+  ok() {
+    this.$.dialog.close();
+    new Audio("/media/buddyup.mp3").play();
   }
 }
 
